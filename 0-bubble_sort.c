19d0f1569e1d62ef768bad515a81f5996b7897e6
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-02-18 20:46:30 +0100

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * @array: array to be sorted
+ * @size: size of the array
+ *
+ * Return: void
+ */
+
+
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i, j, temp;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
